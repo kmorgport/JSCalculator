834ab03d5526f877f7c8bc167a64fb562eb2379e
Kole Morgan <kmorgan0057@gmail.com>
2020-12-20 16:15:37 -0600

    Calc JS Work

--- js/calculator.js
+++ js/calculator.js
@@ -4,4 +4,41 @@ const operation = document.querySelectorAll('[data-operation]');
 const clear = document.querySelector('[data-clear]');
 const equals = document.querySelector('[data-equals]');
 const firstNumer = document.querySelector('[data-first-number]');
-const secNumer = document.querySelector('[data-display-oper]');
+const secondNumer = document.querySelector('[data-second-number]');
+const opdisplay = document.querySelector('[data-display-oper]');
+
+// number.forEach(button=>{
+//     button.addEventListener('click',()=>{
+//         console.log(button.textContent)
+//         firstNumer.textContent = button.textContent
+//     })
+// })
+//
+// var operator = operation.forEach(button=>{
+//     button.addEventListener('click',()=>{
+//         opdisplay.textContent = button.textContent
+//     })
+// })
+
+function math(){
+    let first = number.forEach(button=>{
+        button.addEventListener('click',()=>{
+            console.log(button.textContent)
+            firstNumer.textContent = button.textContent
+        })
+    })
+
+    let op = operation.forEach(button=>{
+        button.addEventListener('click',()=>{
+            opdisplay.textContent = button.textContent
+        })
+    })
+
+    let second = number.forEach(button=>{
+        button.addEventListener('click',()=>{
+            console.log(button.textContent)
+            secondNumer.textContent = button.textContent
+        })
+    })
+}
+math()
