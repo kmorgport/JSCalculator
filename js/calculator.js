17de946dfd5d4284b5b68e2e4b4adafd39371186
Kole Morgan <kmorgan0057@gmail.com>
2020-12-20 12:59:19 -0600

    Calculator JS Functionality

---
+++ js/calculator.js
@@ -0,0 +1,7 @@
+"use strict"
+const number = document.querySelectorAll('[data-number]');
+const operation = document.querySelectorAll('[data-operation]');
+const clear = document.querySelector('[data-clear]');
+const equals = document.querySelector('[data-equals]');
+const firstNumer = document.querySelector('[data-first-number]');
+const secNumer = document.querySelector('[data-display-oper]');
